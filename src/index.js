b8d47bde489270c08173405ebf962f32b5a4f5ef
Marina <lituevam@gmail.com>
2020-02-25 15:39:55 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function toReadable (number) {
+module.exports = function toReadable(number) {
+
+    const arr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 
+    'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 
+    'seventeen', 'eighteen', 'nineteen'];  
   
+    const tens = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+       'eighty', 'ninety'];
+       
+  
+    if (number < 20) {
+       return arr[number];
+    }
+  
+    if (number >= 20 && number < 100) {
+        if (number % 10 === 0) {
+            return tens[number / 10];
+        }
+        else return (tens[Math.floor(number / 10)] + ' ' + arr[number % 10]);
+    }
+  
+    if (number > 99) {
+        if (number % 100 === 0) {
+            return arr[number / 100] + ' ' + 'hundred';
+        }
+  
+        else if (number % 10 === 0) {
+            return (arr[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + 
+            tens[(Math.floor(number / 10)) % 10]);
+        }
+  
+        else if (number % 100 < 20) {
+            return (arr[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' + 
+            arr[number % 100]);
+        }
+  
+        else return (arr[Math.floor(number / 100)] + ' ' + 'hundred' + ' ' +
+        tens[(Math.floor(number / 10)) % 10] + ' ' + arr[number % 10])
+    }
 }
+
